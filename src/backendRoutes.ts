0032a7892a48bc98ef24129b7187597786aa10cf
Moritz R <c0derMo@users.noreply.github.com>
2021-08-21 12:26:34 +0200

    Dynamicly generated menus

--- src/backendRoutes.ts
+++ src/backendRoutes.ts
@@ -63,7 +63,6 @@ const addBackendRoutes = (server) => {
         path: '/backend/competitions',
         handler: async (request, h) => {
             request.log(['get', 'info'], '/backend/competitions');
-            // return h.file("html/backend/matchList.html");
             return await renderBackendPage("matchList", "RR Matches");
         },
         options: {
@@ -76,7 +75,6 @@ const addBackendRoutes = (server) => {
         path: '/backend/players',
         handler: async (request, h) => {
             request.log(['get', 'info'], '/backend/players');
-            // return h.file("html/backend/playerList.html");
             return await renderBackendPage("playerList", "RR Players");
         },
         options: {
@@ -89,7 +87,6 @@ const addBackendRoutes = (server) => {
         path: '/backend/importSpreadsheet',
         handler: async (request, h) => {
             request.log(['get', 'info'], '/backend/importSpreadsheet');
-            // return h.file("html/backend/competitionImport.html");
             return await renderBackendPage("competitionImport", "Import table");
         },
         options: {
@@ -102,7 +99,6 @@ const addBackendRoutes = (server) => {
         path: '/backend/importStandings',
         handler: async (request, h) => {
             request.log(['get', 'info'], '/backend/importStandings');
-            // return h.file('html/backend/standingsImport.html');
             return await renderBackendPage("standingsImport", "Import standings");
         },
         options: {
@@ -128,7 +124,6 @@ const addBackendRoutes = (server) => {
         handler: async (request, h) => {
             request.log(['get', 'info'], '/backend/renamePlayer');
             return await renderBackendPage("renamePlayer", "Rename Player");
-            // return h.file('html/backend/renamePlayer.html');
         },
         options: {
             auth: 'session'
@@ -308,6 +303,90 @@ const addBackendRoutes = (server) => {
             const { oldName, newName } = request.payload;
             request.log(['post', 'info'], '/backend/api/renamePlayer');
             return renamePlayer(oldName, newName);
+        },
+        options: {
+            auth: 'session',
+            plugins: { 'hapi-auth-cookie': { redirectTo: false } } 
+        }
+    })
+
+    server.route({
+        method: 'GET',
+        path: '/backend/api/menu',
+        handler: (request, h) => {
+            request.log(['post', 'info'], '/backend/api/menu');
+            return JSON.stringify([
+                {
+                    "title": "Maintenance mode",
+                    "icon": "eye lash",
+                    "type": "checkbox",
+                    "id": "maintenance-mode",
+                    "description": "Activates maintenance mode for the entire site. The frontend + API will become unavailable."
+                },
+                {
+                    "title": "Competitions",
+                    "icon": "edit",
+                    "type": "link",
+                    "href": "/backend/competitions",
+                    "description": "Lists all the matches of a competition."
+                },
+                {
+                    "title": "Players",
+                    "icon": "edit",
+                    "type": "link",
+                    "href": "/backend/players",
+                    "description": "Lists all the players."
+                },
+                {
+                    "title": "Import spreadsheet",
+                    "icon": "file import",
+                    "type": "link",
+                    "href": "/backend/importSpreadsheet",
+                    "description": "Imports a spreadsheet as a permanent part of the system."
+                },
+                {
+                    "title": "Bulk import standings",
+                    "icon": "file import",
+                    "type": "link",
+                    "href": "/backend/importStandings",
+                    "description": "Import standings and adds them to the corresponding players."
+                },
+                {
+                    "title": "Database checks",
+                    "icon": "stethoscope",
+                    "type": "link",
+                    "href": "/backend/databaseChecks",
+                    "description": "Runs various checks on the database."
+                },
+                {
+                    "title": "Rename player",
+                    "icon": "edit",
+                    "type": "link",
+                    "href": "/backend/renamePlayer",
+                    "description": "Renames a player across all databases."
+                },
+                {
+                    "title": "Reload configs",
+                    "icon": "upload",
+                    "type": "button",
+                    "id": "reload",
+                    "description": "Reloads all the config files from disk."
+                },
+                {
+                    "title": "Recalculate leaderboards",
+                    "icon": "calculator",
+                    "type": "button",
+                    "id": "recalcLeaderboard",
+                    "description": "Recalculate all leaderboard data. (Does not include data from active competition - load a player page instead)"
+                },
+                {
+                    "title": "Shutdown server",
+                    "icon": "power off",
+                    "type": "button",
+                    "id": "shutdown",
+                    "description": "Shuts down the entire server."
+                }
+            ])
         },
         options: {
             auth: 'session',
