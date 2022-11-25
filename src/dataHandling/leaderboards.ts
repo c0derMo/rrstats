134c7a6f9f826df7b0f9d0cc521b460d1b5ada0e
Moritz R <c0derMo@users.noreply.github.com>
2022-11-26 00:32:52 +0100

    Updated leaderboard, renamed & added stats

--- src/dataHandling/leaderboards.ts
+++ src/dataHandling/leaderboards.ts
@@ -10,7 +10,8 @@ interface PlayerRanking {
     won: number;
     mapsPlayed: number[];
     mapsWon: number[];
-    longestWinningSpree: number;
+    mostMatchesWonInARow: number;
+    mostMapsWonInARow: number;
     matchesWithDecider: number;
     grandFinalAppearances: number;
     matchesWithoutGroups: number;
@@ -31,7 +32,8 @@ let leaderboards = {} as PlayerRankings;
 
 export async function recalculate(additionalMatches: RRMatch[]=[], username=""): Promise<void> {
     const rankings = {} as PlayerRankings;
-    const winningSprees = {} as { [key: string]: number };
+    const matchWinningSprees = {} as { [key: string]: number };
+    const mapWinningSprees = {} as { [key: string]: number };
 
     const storedMatches = await database.getRepository(RRMatch).find();
     const matches = [].concat(additionalMatches).concat(storedMatches) as RRMatch[];
@@ -40,11 +42,13 @@ export async function recalculate(additionalMatches: RRMatch[]=[], username=""):
         // Creating default player objects if they don't exist yet
         if(rankings[match.player1] === undefined) {
             rankings[match.player1] = getDefaultRankingObject();
-            winningSprees[match.player1] = 0;
+            matchWinningSprees[match.player1] = 0;
+            mapWinningSprees[match.player1] = 0;
         }
         if(rankings[match.player2] === undefined) {
             rankings[match.player2] = getDefaultRankingObject();
-            winningSprees[match.player2] = 0;
+            matchWinningSprees[match.player2] = 0;
+            mapWinningSprees[match.player2] = 0;
         }
 
         // Amount of RRWC and RR competitions
@@ -69,23 +73,23 @@ export async function recalculate(additionalMatches: RRMatch[]=[], username=""):
         // Winrate & Winningspree
         if(match.score.winner === 1) {
             rankings[match.player1].won += 1;
-            winningSprees[match.player1] += 1;
+            matchWinningSprees[match.player1] += 1;
 
-            rankings[match.player2].longestWinningSpree = Math.max(rankings[match.player2].longestWinningSpree, winningSprees[match.player2])
-            winningSprees[match.player2] = 0;
+            rankings[match.player2].mostMatchesWonInARow = Math.max(rankings[match.player2].mostMatchesWonInARow, matchWinningSprees[match.player2])
+            matchWinningSprees[match.player2] = 0;
         } else if(match.score.winner === 2) {
             rankings[match.player2].won += 1;
-            winningSprees[match.player2] += 1;
+            matchWinningSprees[match.player2] += 1;
 
-            rankings[match.player1].longestWinningSpree = Math.max(rankings[match.player1].longestWinningSpree, winningSprees[match.player1])
-            winningSprees[match.player1] = 0;
+            rankings[match.player1].mostMatchesWonInARow = Math.max(rankings[match.player1].mostMatchesWonInARow, matchWinningSprees[match.player1])
+            matchWinningSprees[match.player1] = 0;
         } else {
             rankings[match.player1].won += 0.5;
             rankings[match.player2].won += 0.5;
-            rankings[match.player2].longestWinningSpree = Math.max(rankings[match.player2].longestWinningSpree, winningSprees[match.player2])
-            winningSprees[match.player2] = 0;
-            rankings[match.player1].longestWinningSpree = Math.max(rankings[match.player1].longestWinningSpree, winningSprees[match.player1])
-            winningSprees[match.player1] = 0;
+            rankings[match.player2].mostMatchesWonInARow = Math.max(rankings[match.player2].mostMatchesWonInARow, matchWinningSprees[match.player2])
+            matchWinningSprees[match.player2] = 0;
+            rankings[match.player1].mostMatchesWonInARow = Math.max(rankings[match.player1].mostMatchesWonInARow, matchWinningSprees[match.player1])
+            matchWinningSprees[match.player1] = 0;
         }
 
         // Per Map Stats
@@ -102,59 +106,57 @@ export async function recalculate(additionalMatches: RRMatch[]=[], username=""):
                 rankings[match.player1].ownMaps += 1
                 rankings[match.player2].opponentMaps += 1
                 if(map.winner === 1) {
-                    rankings[match.player1].mapsWon[mapAbbreviationToArrayIndex(map.map)] += 1
-                    rankings[match.player1].mapsWonAmount += 1
                     rankings[match.player1].ownMapsWon += 1
                 } else if(map.winner === 2) {
-                    rankings[match.player2].mapsWon[mapAbbreviationToArrayIndex(map.map)] += 1
-                    rankings[match.player2].mapsWonAmount += 1
                     rankings[match.player2].opponentMapsWon += 1
                 } else {
-                    rankings[match.player2].mapsWon[mapAbbreviationToArrayIndex(map.map)] += 0.5
-                    rankings[match.player2].mapsWonAmount += 0.5
                     rankings[match.player2].opponentMapsWon += 0.5
-                    rankings[match.player1].mapsWon[mapAbbreviationToArrayIndex(map.map)] += 0.5
-                    rankings[match.player1].mapsWonAmount += 0.5
                     rankings[match.player1].ownMapsWon += 0.5
                 }
             } else if(map.pickedBy === 2) {
                 rankings[match.player2].ownMaps += 1
                 rankings[match.player1].opponentMaps += 1
                 if(map.winner === 1) {
-                    rankings[match.player1].mapsWon[mapAbbreviationToArrayIndex(map.map)] += 1
-                    rankings[match.player1].mapsWonAmount += 1
                     rankings[match.player1].opponentMapsWon += 1
                 } else if(map.winner === 2) {
-                    rankings[match.player2].mapsWon[mapAbbreviationToArrayIndex(map.map)] += 1
-                    rankings[match.player2].mapsWonAmount += 1
                     rankings[match.player2].ownMapsWon += 1
                 } else {
-                    rankings[match.player1].mapsWon[mapAbbreviationToArrayIndex(map.map)] += 0.5
-                    rankings[match.player1].mapsWonAmount += 0.5
                     rankings[match.player1].opponentMapsWon += 0.5
-                    rankings[match.player2].mapsWon[mapAbbreviationToArrayIndex(map.map)] += 0.5
-                    rankings[match.player2].mapsWonAmount += 0.5
                     rankings[match.player2].ownMapsWon += 0.5
                 }
+            }
+            
+            if (map.winner === 1) {
+                rankings[match.player1].mapsWon[mapAbbreviationToArrayIndex(map.map)] += 1
+                rankings[match.player1].mapsWonAmount += 1
+
+                mapWinningSprees[match.player1] += 1;
+                rankings[match.player2].mostMapsWonInARow = Math.max(rankings[match.player2].mostMapsWonInARow, mapWinningSprees[match.player2]);
+                mapWinningSprees[match.player2] = 0;
+            } else if (map.winner === 2) {
+                rankings[match.player2].mapsWon[mapAbbreviationToArrayIndex(map.map)] += 1
+                rankings[match.player2].mapsWonAmount += 1
+
+                mapWinningSprees[match.player2] += 1;
+                rankings[match.player1].mostMapsWonInARow = Math.max(rankings[match.player1].mostMapsWonInARow, mapWinningSprees[match.player1]);
+                mapWinningSprees[match.player1] = 0;
             } else {
-                if(map.winner === 1) {
-                    rankings[match.player1].mapsWon[mapAbbreviationToArrayIndex(map.map)] += 1
-                    rankings[match.player1].mapsWonAmount += 1
-                } else if(map.winner === 2) {
-                    rankings[match.player2].mapsWon[mapAbbreviationToArrayIndex(map.map)] += 1
-                    rankings[match.player2].mapsWonAmount += 1
-                } else {
-                    rankings[match.player1].mapsWon[mapAbbreviationToArrayIndex(map.map)] += 0.5
-                    rankings[match.player1].mapsWonAmount += 0.5
-                    rankings[match.player2].mapsWon[mapAbbreviationToArrayIndex(map.map)] += 0.5
-                    rankings[match.player2].mapsWonAmount += 0.5
-                }
+                rankings[match.player1].mapsWon[mapAbbreviationToArrayIndex(map.map)] += 0.5
+                rankings[match.player1].mapsWonAmount += 0.5
+                rankings[match.player2].mapsWon[mapAbbreviationToArrayIndex(map.map)] += 0.5
+                rankings[match.player2].mapsWonAmount += 0.5
+
+                rankings[match.player1].mostMapsWonInARow = Math.max(rankings[match.player1].mostMapsWonInARow, mapWinningSprees[match.player1]);
+                mapWinningSprees[match.player1] = 0;
+                rankings[match.player2].mostMapsWonInARow = Math.max(rankings[match.player2].mostMapsWonInARow, mapWinningSprees[match.player2]);
+                mapWinningSprees[match.player2] = 0;
             }
         }
     }
 
-    for(const player in winningSprees) {
-        rankings[player].longestWinningSpree = Math.max(rankings[player].longestWinningSpree, winningSprees[player])
+    for(const player in matchWinningSprees) {
+        rankings[player].mostMatchesWonInARow = Math.max(rankings[player].mostMatchesWonInARow, matchWinningSprees[player])
+        rankings[player].mostMapsWonInARow = Math.max(rankings[player].mostMapsWonInARow, mapWinningSprees[player])
     }
 
     leaderboards = rankings;
@@ -191,6 +193,21 @@ export function getLeaderboardStat(stat: string, map=""): object {
                 return (obj.rankings[b].matches) - (obj.rankings[a].matches)
             });
             break;
+        case 'MatchesWon':
+            obj.order.sort((a, b) => {
+                return (obj.rankings[b].won) - (obj.rankings[a].won)
+            });
+            break;
+        case 'Maps':
+            obj.order.sort((a, b) => {
+                return (obj.rankings[b].mapsPlayedAmount) - (obj.rankings[a].mapsPlayedAmount)
+            });
+            break;
+        case 'MapsWon':
+            obj.order.sort((a, b) => {
+                return (obj.rankings[b].mapsWonAmount) - (obj.rankings[a].mapsWonAmount)
+            });
+            break;
         case 'PercentageDecider':
             //TODO: Broken because of the calculation (see L41)
             obj.order = obj.order.filter(function(e) {
@@ -206,9 +223,14 @@ export function getLeaderboardStat(stat: string, map=""): object {
                 return (obj.rankings[b].grandFinalAppearances) - (obj.rankings[a].grandFinalAppearances)
             });
             break;
-        case 'LongestWinSpree':
+        case 'MostMatchesWonInARow':
             obj.order.sort((a, b) => {
-                return (obj.rankings[b].longestWinningSpree) - (obj.rankings[a].longestWinningSpree)
+                return (obj.rankings[b].mostMatchesWonInARow) - (obj.rankings[a].mostMatchesWonInARow)
+            });
+            break;
+        case 'MostMapsWonInARow':
+            obj.order.sort((a, b) => {
+                return (obj.rankings[b].mostMapsWonInARow) - (obj.rankings[a].mostMapsWonInARow)
             });
             break;
         case 'MapPlayed':
@@ -264,7 +286,8 @@ function getDefaultRankingObject(): PlayerRanking {
         won: 0,
         mapsPlayed: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
         mapsWon: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        longestWinningSpree: 0,
+        mostMatchesWonInARow: 0,
+        mostMapsWonInARow: 0,
         matchesWithDecider: 0,
         grandFinalAppearances: 0,
         matchesWithoutGroups: 0,
