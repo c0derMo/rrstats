026b11404a3aa322e1fadd84a4fad905234d209c
Moritz R <c0derMo@users.noreply.github.com>
2023-11-09 01:27:29 +0100

    Add consenting dialog to light dark switch

--- app.vue
+++ app.vue
@@ -3,6 +3,8 @@
     <div :class="{ 'dark': isDarkMode }" class="w-full h-full">
         <div class="dark:text-white min-w-screen min-h-screen w-full h-full flex flex-col">
             <div class="dark:bg-slate-900 fixed w-full h-full -z-50"></div>
+            <LocalStorageConsentDialog v-if="showForm" @decline="showForm = false" @consent="consentToLocalStorage" />
+
             <NuxtLayout>
                 <template #BackButton>
                     <FontAwesomeIcon
@@ -18,7 +20,7 @@
                             :icon="['fas', 'sun']"
                             class="text-white"
                         />
-                        <SwitchComponent v-model="isDarkMode" id="light-dark" />
+                        <SwitchComponent v-model="lightDarkSwitch" id="light-dark" />
                         <FontAwesomeIcon
                             :icon="['fas', 'moon']"
                             class="ml-3 text-white"
@@ -43,11 +45,39 @@ library.add(faMoon);
 library.add(faChevronCircleLeft);
 
 const isDarkMode = ref(true);
+const lightDarkSwitch = ref(true);
+const showForm = ref(false);
+const initialized = ref(false);
+
 onMounted(() => {
-    isDarkMode.value = (window.localStorage.getItem('theme') === 'dark') || (window.localStorage.getItem('theme') === null && window.matchMedia('(prefers-color-scheme: dark)').matches)
+    lightDarkSwitch.value = (window.localStorage.getItem('theme') === 'dark') || (window.localStorage.getItem('theme') === null && window.matchMedia('(prefers-color-scheme: dark)').matches);
+    isDarkMode.value = (window.localStorage.getItem('theme') === 'dark') || (window.localStorage.getItem('theme') === null && window.matchMedia('(prefers-color-scheme: dark)').matches);
+    nextTick(() => { initialized.value = true });
 })
 
 watch(isDarkMode, () => {
+    if (!initialized.value) {
+        return;
+    }
     window.localStorage.setItem('theme', isDarkMode.value ? 'dark' : 'light');
 });
+
+watch(lightDarkSwitch, (newValue, oldValue) => {
+    if (showForm.value || !initialized.value) {
+        return;
+    }
+    if (window.localStorage.getItem('consent') === null) {
+        showForm.value = true;
+        nextTick(() => {
+            lightDarkSwitch.value = oldValue;
+        });
+    } else {
+        isDarkMode.value = newValue;
+    }
+})
+
+function consentToLocalStorage() {
+    window.localStorage.setItem('consent', 'consented');
+    showForm.value = false;
+}
 </script>
