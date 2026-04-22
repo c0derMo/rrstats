fb401ac054dd2bd639c3bac3d35abfd8e42260f2
Moritz R <c0derMo@users.noreply.github.com>
2026-04-22 17:28:53 +0200

    revert hashNavigation logic

--- app/composables/hashNavigation.ts
+++ app/composables/hashNavigation.ts
@@ -2,9 +2,9 @@ type HashNavigationFunction = (hash: string[]) => Promise<void> | void;
 
 export const useHash = (func: HashNavigationFunction) => {
     const route = useRoute();
-    const router = useRouter();
     const hasherData = useState("hash", () => {
         return {
+            ignoreNext: false,
             lastClicked: "",
         };
     });
@@ -17,19 +17,22 @@ export const useHash = (func: HashNavigationFunction) => {
         await navigateTo({ ...route, hash: "" }, { replace: true });
     });
 
-    const removeHandler = router.afterEach(async (to, from) => {
-        if (to.path === from.path) {
-            return;
-        }
-        await func(to.hash.split("."));
-        await navigateTo({ ...to, hash: "" }, { replace: true });
-    });
+    watch(
+        () => route.hash,
+        async () => {
+            if (route.hash === "") {
+                return;
+            }
+            if (hasherData.value.ignoreNext) {
+                hasherData.value.ignoreNext = false;
+                return;
+            }
+            await func(route.hash.split("."));
+            await navigateTo({ ...route, hash: "" }, { replace: true });
+        },
+    );
 
-    onBeforeUnmount(() => {
-        removeHandler();
-    });
-
-    return (hash: string, immediate: boolean = false): void => {
+    return (hash: string, immediate: boolean = false) => {
         if (!hash.startsWith("#")) {
             console.error(
                 `Trying to set hash that doesnt start with #, aborting (${hash})`,
@@ -37,8 +40,9 @@ export const useHash = (func: HashNavigationFunction) => {
             return;
         }
         if (hasherData.value.lastClicked === hash || immediate) {
+            hasherData.value.ignoreNext = true;
             window.location.hash = hash;
-            navigateTo({ ...route, hash: hash }, { replace: true });
+            return navigateTo({ ...route, hash: hash }, { replace: true });
         } else {
             hasherData.value.lastClicked = hash;
         }
